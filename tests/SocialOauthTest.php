1143c383b288ecbd239d395fcaff6610d589df25
Stavros Zavrakas <stavros.zavrakas@styloola.com>
2014-07-03 11:29:08 +0200

    fix composer and first test

--- tests/SocialOauthTest.php
+++ tests/SocialOauthTest.php
@@ -1,8 +1,18 @@
 <?php
 
     class SocialOauthTest extends PHPUnit_Framework_TestCase {
-
+        
+        private $social_oauth_consumer = null;
+        
         public function setUp() {
-            
+            $this->social_oauth_consumer = new SocialOauth\SocialOauth;
+        }
+
+        public function tearDown() {
+            $this->social_oauth_consumer = null;
+        }
+
+        public function testInstanceOf() {
+            $this->assertInstanceOf('SocialOauth\SocialOauth', $this->social_oauth_consumer);
         }
     }
