925d3dbd81a1bb542fc883002b919d671ea3a4bb
Stavros Zavrakas <stavros.zavrakas@styloola.com>
2014-07-04 11:58:37 +0200

    config added for other social networks

--- src/config/config.php
+++ src/config/config.php
@@ -4,12 +4,30 @@ return array(
 
     /*
     |--------------------------------------------------------------------------
-    | social configuration file
+    | social oauth configuration file
     |--------------------------------------------------------------------------
     */
+    
     'facebook' => array(
         'client_id' => null,
         'client_secret' => null,
         'scope' => null,
+    ),
+
+    'google' => array(
+        'client_id' => null,
+        'client_secret' => null,
+        'scope' => null,
+    ),
+
+    'linkedin' => array(
+        'client_id' => null,
+        'client_secret' => null,
+        'scope' => null,
+    ),
+
+    'eventbrite' => array(
+        'client_id' => null,
+        'client_secret' => null,
     ),
 );
